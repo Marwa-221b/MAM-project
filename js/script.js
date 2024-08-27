d7dabd27c647d5e76384fe958cc5dd5d3df8d924
Mariam <mariam.abd6606@gmail.com>
2024-08-27 09:26:55 +0300

    combined sections

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-let observer = new IntersectionObserver((entries, observer)=>{
+let observer = new IntersectionObserver((entries, observer) => {
     entries.filter(e => e.isIntersecting).forEach(entry => {
         entry.target.classList.add("show");
         observer.unobserve(entry.target);
     });
 });
-document.querySelectorAll('.hidden').forEach((el)=> observer.observe(el )) ;
+document.querySelectorAll('.hidden').forEach((el) => observer.observe(el));
